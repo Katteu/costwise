2003fa927fd0cec5f31e5b1affc33d48d38e88a6
Katteu <katheamari@gmail.com>
2024-10-15 07:12:45 +0800

    add cors

---
+++ api/config/cors.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'paths' => ['*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['https://88.222.214.127', 'https://costwise-eight.vercel.app/'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true,
+];
