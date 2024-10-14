3c7b15ad8d53d1d071815665cf0799496f83b123
Katteu <katheamari@gmail.com>
2024-10-15 07:15:03 +0800

    fix: cors again

--- api/config/cors.php
+++ api/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://88.222.214.127', 'https://costwise-eight.vercel.app/'],
+    'allowed_origins' => ['https://88.222.214.127', 'https://costwise-eight.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
